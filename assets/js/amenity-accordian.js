2e67cfcd79be47b41b3b550ae19a35c0dd181758
Matt McCullough <mmccullough@blackjet.ca>
2016-11-25 17:32:48 -0500

    Showing cat parents, injecting amenities into sublists

---
+++ assets/js/amenity-accordian.js
@@ -0,0 +1,37 @@
+var $ = jQuery;
+var cat_accordian = {
+
+	amentityCategories: [],
+	returnHtml: '',
+
+	init: function(args) {
+		var amentityCategories = CATEGORY_AMENITIES.categories;
+		this.amentityCategories = amentityCategories;
+		this.accordianList = args.accordianList;
+		this.loopHtml();
+	},
+
+	loopHtml: function() {
+		var amentityCategories = this.amentityCategories;
+		for( var i in amentityCategories ) {
+			 this.setAmenityList( amentityCategories[i] );
+		}
+	},
+
+	setAmenityList: function( categoryAmenityList ) {
+		 
+		 // console.log(categoryAmenityList);
+
+		var $catParent = this.accordianList.find('#amenity-category-' + categoryAmenityList.category.term_id );
+		for( var i in categoryAmenityList.amenities ) {
+			 
+			 // console.log(categoryAmenityList.amenities);
+			 
+			 var amenity = categoryAmenityList.amenities[i];
+			 var html = '';
+			 html += '<li class="amenity-list-item amenity-' + amenity.ID  + '">' + amenity.post_title + '<li>';
+		}
+		$catParent.find('ul').html(html);
+	}
+
+}
