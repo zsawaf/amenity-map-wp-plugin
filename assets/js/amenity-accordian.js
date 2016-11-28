41e700fcf9b1c99710c8c52b8580687238a60ed3
zsawaf <zsawaf@blackjet.ca>
2016-11-28 11:01:59 -0500

    Working on accordion

--- assets/js/amenity-accordian.js
+++ assets/js/amenity-accordian.js
@@ -24,17 +24,33 @@ var cat_accordian = {
 	},
 
 	setAmenityList: function( categoryAmenityList ) {
-		 
+
+		var self = this;
 		var $catParent = this.accordianList.find('#amenity-category-' + categoryAmenityList.category.term_id );
-		for( var i in categoryAmenityList.amenities ) {
-			 
-			 // console.log(categoryAmenityList.amenities);
-			 
-			 var amenity = categoryAmenityList.amenities[i];
-			 var html = '';
-			 html += '<li class="amenity-list-item amenity-' + amenity.ID  + '">' + amenity.post_title + '<li>';
+		for( var i in categoryAmenityList.amenities ) {	
+			// console.log(categoryAmenityList.amenities);
+			var amenity = categoryAmenityList.amenities[i];
+			var html = '';
+			html += '<li class="amenity-list-item amenity-' + amenity.ID  + '">' + amenity.post_title + '<li>';
+			var place = self.getPlaceId(amenity);
 		}
 		$catParent.find('ul').html(html);
+	},
+
+	getPlaceId: function(amenity) {
+		var place_id = amenity.place_id;
+		var self = this;
+
+		var service = new google.maps.places.PlacesService(document.createElement('div'));
+		console.log($(".amenity-list-item-"+amenity.ID)[0]);
+		var html;
+		service.getDetails({
+		  placeId: place_id
+		}, function(place, status) {
+			if (status === google.maps.places.PlacesServiceStatus.OK) {
+				console.log(place);
+			}
+		});
 	}
 
 }
